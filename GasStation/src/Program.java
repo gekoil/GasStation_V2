e2dbb5e94f28a837675607e1bf280dd31a0a05c2
Jack <jstrongin@gmail.com>
2015-06-02 22:06:46 +0300

    Separated JavaFX from main Program Class

--- GasStation/src/Program.java
+++ GasStation/src/Program.java
@@ -1,71 +1,10 @@
-
-
-import BL.CreateGsFromXML;
-import BL.GasStation;
-import Controller.GasStationController;
+import BL.GasStationServer;
 import javafx.application.Application;
-import javafx.event.EventHandler;
-import javafx.geometry.Pos;
-import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.control.TabPane;
-import javafx.scene.control.TabPane.TabClosingPolicy;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.text.Text;
-import javafx.stage.Stage;
-import javafx.stage.WindowEvent;
-import UI.CarCreatorPane;
-import UI.FuelPane;
-import UI.UIStatistics;
-import Views.MainFuelAbstractView;
 
-public class Program extends Application {
-	
-	private GasStation gs;
-	private static FuelPane fuelPane;
-	private static CarCreatorPane carPane;
-	private Text headline;
-	private static UIStatistics stat;
-	GasStationController fuelCtrl;
-	private final String BILD_DATA = "input.xml";
-	
+public class Program {
+
 	public static void main(String[] args) {
-		launch(args);
+		Application.launch(GasStationServer.class, args);
 	} // main
-	
-	@Override
-	public void start(Stage primaryStage) throws Exception {
-		CreateGsFromXML creator = new CreateGsFromXML(BILD_DATA);
-		gs = creator.CreatGasStation();
-		primaryStage.setScene(creatScene());
-		primaryStage.setTitle("Gas Station");
-		
-		fuelCtrl = new GasStationController(gs, fuelPane, stat, carPane);
-		
-		primaryStage.setMinHeight(500);
-		primaryStage.setMinWidth(600);
-		primaryStage.show();
-	}
-	
-	public Scene creatScene() {
-		BorderPane border = new BorderPane();
-		Scene scene = new Scene(border);
-		scene.getStylesheets().add(Program.class.getResource("/UI/Style.css").toExternalForm());
-		
-		fuelPane = new FuelPane();
-		fuelPane.setId("mainFuelBox");
-		stat = new UIStatistics();
-		headline = new Text("WELCOME TO THE GAS STATION");
-		headline.setId("headline");
-		carPane = new CarCreatorPane(gs.getPumps().length);
-		
-		border.setAlignment(headline, Pos.CENTER);
-		border.setTop(headline);
-		border.setBottom(fuelPane);
-		border.setCenter(stat);
-		border.setRight(carPane);
-		return scene;
-	}
-
 	
 }  // Program
